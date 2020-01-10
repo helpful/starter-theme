6b7a363c78c8cc7fae16bd9111f87b7e2ffa5d41
Phil Banks <phil@helpfuldigital.com>
2020-01-10 15:21:23 +0000

    better starter post type/tax code

--- inc/post-types-taxonomies.php
+++ inc/post-types-taxonomies.php
@@ -17,48 +17,65 @@
 /*add_action( 'init', function () {
 
 	register_extended_post_type(
-		'resource',
+		'event',
 		[
-			'has_archive'     => false,
-			'capability_type' => 'post',
-			'menu_icon'       => 'dashicons-media-text',
-			'supports'        => [ 'title', 'editor', 'excerpt', 'thumbnail', 'revisions' ],
-			'admin_cols'      => [
-				'xxx'         => [
-					'title'    => 'Xxx',
-					'taxonomy' => 'xxx',
+			'has_archive'      => false,
+			'capability_type'  => 'post',
+			'menu_icon'        => 'dashicons-calendar-alt',
+			'supports'         => [ 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author' ],
+			'delete_with_user' => false,
+			//'rewrite'        => [
+			//	'permastruct' => '/xxx/%event%' // Set xxx if you need it to be different to the slug.
+			//],
+			'admin_cols'       => [
+				'logo' => [
+					'title'          => 'Logo',
+					'featured_image' => 'thumbnail', // Image size to use.
+					'width'          => 50,
+					'height'         => 50,
 				],
-				'xxx'          => [
+				'location'         => [
+					'title'    => 'Location',
+					'taxonomy' => 'location',
+				],
+				'event_date' => [
+					'title'    => 'Event date',
+					'meta_key' => 'event_date',
+				],
+				'date'          => [
+					'title'      => 'Publish date',
 					'post_field' => 'post_date',
 					'default'    => 'DESC',
 				],
 			],
 			'admin_filters'   => [
-				'xxx'         => [
-					'title'    => 'Xxx',
-					'taxonomy' => 'xxx',
+				'location'         => [
+					'title'    => 'Location',
+					'taxonomy' => 'location',
 				],
 			],
 		],
 		[
-			'singular' => 'Xxx',
-			'plural'   => 'Xxxs',
-			'slug'     => 'xxx'
+			'singular' => 'Event',
+			'plural'   => 'Events',
+			'slug'     => 'event'
 		]
 	);
 
 	register_extended_taxonomy(
-		'xxx', // Slug.
+		'location', // Slug.
 		[
-			'xxx', // Post type(s) to apply to.
+			'event', // Post type(s) to apply to.
 		],
 		[
+			//'meta_box'     => 'radio', // Set to customise interface - options: false, 'simple', 'radio', 'dropdown' - latter 2 allow single value to be selected only.
 			'hierarchical' => false,
+			//'required'     => false // Set to enforce selection.
 		],
 		[
-			'singular' => 'Xxx',
-			'plural'   => 'Xxxs',
-			'slug'     => 'xxx',
+			'singular' => 'Location',
+			'plural'   => 'Locations',
+			'slug'     => 'location',
 		]
 	);
 
@@ -68,7 +85,7 @@
 
 
 /**
- * Change dashboard Posts to News
+ * Re-label Posts as News in wp-admin.
  */
 /*function starter_theme_change_post_labels() {
 	$get_post_type              = get_post_type_object( 'post' );
@@ -88,3 +105,34 @@
 	$labels->name_admin_bar     = 'News';
 }
 add_action( 'init', 'starter_theme_change_post_labels' );*/
+
+
+
+
+/**
+ * Re-label Tags to Themes in wp-admin (to match front-end).
+ */
+/*function starter_theme_label_tags_themes() {
+	global $wp_taxonomies;
+	$labels = &$wp_taxonomies['post_tag']->labels;
+	$labels->name = 'Themes';
+	$labels->singular_name = 'Themes';
+	$labels->search_items = 'Search themes';
+	$labels->all_items = 'Themes';
+	$labels->separate_items_with_commas = 'Separate themes with commas';
+	$labels->choose_from_most_used = 'Choose from the most used themes';
+	$labels->popular_items = 'Popular themes';
+	$labels->edit_item = 'Edit theme';
+	$labels->view_item = 'View theme';
+	$labels->update_item = 'Update theme';
+	$labels->add_new_item = 'Add new theme';
+	$labels->new_item_name = 'New theme';
+	$labels->add_or_remove_items = 'Add or remove theme';
+	$labels->not_found = 'Theme not found';
+	$labels->no_terms = 'No themes';
+	$labels->items_list_navigation = 'Themes list navigation';
+	$labels->items_list = 'List of themes';
+	$labels->back_to_items = '← Back to themes';
+	$labels->menu_name = 'Themes';
+}
+add_action( 'init', 'starter_theme_label_tags_themes' );*/
