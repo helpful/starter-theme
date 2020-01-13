0dd01e9fd7b59feafb60064a85b46843e6cae25d
Phil Banks <phil@helpfuldigital.com>
2020-01-13 22:19:36 +0000

    remove some unused add_filter/action calls

--- functions.php
+++ functions.php
@@ -60,9 +60,6 @@ class starter_theme_Site extends Timber\Site {
 	public function __construct() {
 		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
 		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
-		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
-		add_action( 'init', array( $this, 'register_taxonomies' ) );
 		parent::__construct();
 	}
 
